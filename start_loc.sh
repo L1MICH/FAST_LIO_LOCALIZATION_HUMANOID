fd409ce1f03b20c7b7e0917408e8ad1b22f4bc9a
L1MICH233 <593410663@qq.com>
2026-01-29 10:42:14 +0800

    添加自动化脚本

---
+++ start_loc.sh
@@ -0,0 +1,3 @@
+source livox_ros_driver/devel/setup.bash
+source loc/devel/setup.bash
+roslaunch open3d_loc localization_mid360_g1.launch
---
+++ start_mapping.sh
@@ -0,0 +1,3 @@
+source livox_ros_driver/devel/setup.bash
+source loc/devel/setup.bash
+roslaunch fast_lio mapping_mid360_g1.launch
